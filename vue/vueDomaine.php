bcf96e4a00830137e92c922e4f23b69ff36771c0
Théophile <theo.hamelin49@gmail.com>
2018-11-30 08:29:36 +0100

    Map

--- vue/vueDomaine.php
+++ vue/vueDomaine.php
@@ -64,6 +64,35 @@ class vueDomaine {
 			<?php include 'includes/headHTML.php' ?>
 		</head>
 		<body onload="initMap();">
+		<p id="locationphp" value="<?php
+
+					require_once "./config/config.php";
+
+				//	header("Access-Control-Allow-Origin: *");
+
+					try {
+							$chaine = "mysql:host=".HOST.";dbname=".BD.";charset=UTF8";
+							$db = new PDO($chaine,LOGIN,PASSWORD);
+							$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+					} catch (PDOException $e) {
+							throw new PDOException("Erreur de connexion");
+					}
+					if (!isset($_SESSION['id'])) {
+						$Paris = "48.866667, 2.333333";
+						echo $Paris;
+					} else {
+						$ID = $_SESSION['id'];
+						$sql_get_location = "SELECT location FROM Utilisateurs WHERE mail=:id";
+						$sth = $db->prepare($sql_get_location);
+						$sth->bindParam(":id", $ID);
+						$sth->execute();
+						$reponse = $sth->fetch(PDO::FETCH_ASSOC);
+
+						foreach($reponse as $result) {
+								echo $result;
+						}
+					}
+				?>"></p>
 			<!--  HEADER-->
 			<?php  include 'includes/header.php' ?>
 
